93a2690cd9e51abb3f5dac9f6b437263ac7bd4fd
Daan <Daantj3uh@hotmail.com>
2015-04-03 19:17:26 +0200

    Log-in scherm

--- ProjectChallenge/MainWindow.xaml.cs
+++ ProjectChallenge/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,18 +11,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
-namespace ProjectChallenge
+//Daan Theunis
+//3.04.2015
+namespace Login_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    /// </summary> 
+
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-            Nou dit is een testje
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            
+            }
+        }
+
+
     }
-}
