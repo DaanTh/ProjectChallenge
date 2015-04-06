661dd7a29304394baaf1a018879e0f7b84f20801
Daan <Daantj3uh@hotmail.com>
2015-04-06 16:04:38 +0200

    Revert "test"
    This reverts commit cd655f1a2cc631417a8ad9ea85aa857ad991a849.
    
    Merge branch 'master' of https://github.com/DaanTh/ProjectChallenge
    
    test

---
+++ ProjectChallenge/Kennis.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+// author : Theunis Daan
+// Date : 04.04.2015 17:31
+namespace ProjectChallenge
+{
+    /// <summary>
+    /// Interaction logic for Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 menu = new Window2();
+            menu.Show();
+            this.Close();
+        }
+        private void eerste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tweede_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void derde_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tafels_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void optellingen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void delingen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ ProjectChallenge/Leerstofselectie.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+//Author : Daan Theunis
+//Date : 04.04.2015 17:20
+
+namespace ProjectChallenge
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void wiskunde_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 wiskunde = new Window3();
+            wiskunde.Show();
+            this.Close(); ;
+        }
+        private void taal_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 wiskunde = new Window4();
+            wiskunde.Show();
+            this.Close(); ;
+        }
+        private void kennis_Click(object sender, RoutedEventArgs e)
+        {
+            Window5 wiskunde = new Window5();
+            wiskunde.Show();
+            this.Close(); ;
+        }
+    }
+}
